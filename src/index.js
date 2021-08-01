7b28d494cc032d8ae8eb98af3da9e26f6e3a2976
MingFan789 <mingfan789@gmail.com>
2021-08-02 01:20:39 +0800

    modified index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const multiButton = () => {
+  let output = [];
+  for (let i = 0; i < 4; ++i) output.push(<button>Button{i}</button>);
+  return output;
+};
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>Hello world!</h1>
     <App />
+    {multiButton()}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
